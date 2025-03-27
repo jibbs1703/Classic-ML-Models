f78d1418f03900d57eda49c7a8b3ea7ac0210727
Abraham Ajibade <97408939+jibbs1703@users.noreply.github.com>
2025-03-26 20:50:42 -0400

    completed the adult income deployment and created prediction endpoint (#4)
    This merge:
    - completes the training of the adult income model
    - adds a simple fastapi backend to get predictions
    - adds a descriptive project description for the adult income model

--- src/adult_income/predict/app.py
+++ src/adult_income/predict/app.py
@@ -10,62 +10,53 @@ from fastapi import FastAPI, HTTPException
 from models import IncomeInput
 
 from adult_income import config
-from src.utils.aws.s3 import S3Buckets
+from utils.aws.s3 import S3Buckets
 
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 s3_client = S3Buckets.credentials()
 
-app = FastAPI(
-    title="Adult Income Classifier",
-    description="Predict income level (<=50K or >50K) based on user inputs.",
-    version="1.0.0"
-)
+ml_models ={}
 
-model = None
-scaler = None
-encoder = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    """Manage startup and shutdown events for the FastAPI app."""
-    # Startup: Load resources from S3
-    global model, scaler, encoder
-    try:
-        logger.info("Loading resources from S3 on startup...")
-        model = s3_client.load_model_from_s3(
-            bucket_name=config.CLEAN_BUCKET_NAME,
-            object_key=config.MODEL_NAME,
-            folder=config.ARTIFACT_FOLDER
-        )
-        scaler = s3_client.load_model_from_s3(
-            bucket_name=config.CLEAN_BUCKET_NAME,
-            object_key=config.SCALER_NAME,
-            folder=config.ARTIFACT_FOLDER
-        )
-        encoder = s3_client.load_model_from_s3(
-            bucket_name=config.CLEAN_BUCKET_NAME,
-            object_key=config.ENCODER_NAME,
-            folder=config.ARTIFACT_FOLDER
-        )
-        logger.info("Resources loaded successfully.")
-    except Exception as e:
-        logger.error(f"Failed to load resources from S3: {str(e)}")
-        raise
+    logger.info("Starting application and loading resources from S3...")
+    ml_models["model"] = s3_client.load_model_from_s3(
+        bucket_name=config.CLEAN_BUCKET_NAME,
+        model_name=config.MODEL_NAME,
+        folder=config.ARTIFACT_FOLDER
+    )
+    ml_models["scaler"] = s3_client.load_model_from_s3(
+        bucket_name=config.CLEAN_BUCKET_NAME,
+        model_name=config.SCALER_NAME,
+        folder=config.ARTIFACT_FOLDER
+    )
+    ml_models["encoder"] = s3_client.load_model_from_s3(
+        bucket_name=config.CLEAN_BUCKET_NAME,
+        model_name=config.ENCODER_NAME,
+        folder=config.ARTIFACT_FOLDER
+    )
     yield
-    logger.info("Shutting down app...")
+    logger.info("Shutting down application and clearing resources...")
+    ml_models.clear()
 
 
-app.lifespan = lifespan
+app = FastAPI(
+    title="Adult Income Classifier",
+    description="Predict income level (<=50K or >50K) based on user inputs.",
+    version="1.0.0",
+    lifespan=lifespan,
+)
 
 
 @app.get("/health")
 async def health_check():
     """Check if the app and resources are ready."""
-    if model is None or scaler is None or encoder is None:
+    if ml_models["model"] is None and ml_models["scaler"] is None and ml_models["encoder"] is None:
         raise HTTPException(status_code=503, detail="One or More Resources not loaded.")
-    return {"status": "healthy", "message": "Model, scaler, and encoder are loaded."}
+    return {"status": "healthy", "message": "model, scaler, and encoder are loaded."}
 
 
 @app.get("/")
@@ -73,7 +64,7 @@ async def read_root():
     return {"message": "Welcome to the Adult Income Classifier API!"}
 
 
-@app.post("/predict/", response_model=dict)
+@app.post("/predict", response_model=dict)
 async def predict_income(input_data: IncomeInput):
     """Predict income classification based on user inputs.
 
@@ -83,20 +74,22 @@ async def predict_income(input_data: IncomeInput):
     Returns:
         dict: Prediction result with income class and probability.
     """
-    if model is None or scaler is None or encoder is None:
+    if ml_models["model"] is None or ml_models["scaler"] is None or ml_models["encoder"] is None:
         raise HTTPException(status_code=503, detail="One or More Resources not loaded.")
 
-    input_df = pd.DataFrame([input_data.dict()])
+    input_df = pd.DataFrame([input_data.model_dump()])
     input_df = input_df[config.NUMERIC_COLUMNS + config.CATEGORICAL_COLUMNS]
 
-    input_df[config.NUMERIC_COLUMNS] = scaler.transform(input_df[config.NUMERIC_COLUMNS])
-    input_df[config.CATEGORICAL_COLUMNS] = encoder.transform(input_df[config.CATEGORICAL_COLUMNS])
+    input_df[config.NUMERIC_COLUMNS] = ml_models["scaler"].transform(
+        input_df[config.NUMERIC_COLUMNS])
+    input_df[config.CATEGORICAL_COLUMNS] = ml_models["encoder"].transform(
+        input_df[config.CATEGORICAL_COLUMNS])
 
-    prediction = model.predict(input_df)
-    probability = model.predict_proba(input_df)
+    prediction = ml_models["model"].predict(input_df)
+    probability = ml_models["model"].predict_proba(input_df)
 
     return {"prediction": prediction[0], "probability": np.max(probability)}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8008)
